8eafb1f0ea82c9b2ea69b6b31b788665169dac87
Edward He <eh538@cornell.edu>
2023-03-19 14:43:00 -0400

    Updated pet.ml and pet.mli - Added new type pets to store pets - Added new function pets_of_json which outputs a pet list

--- src/pet.mli
+++ src/pet.mli
@@ -1,9 +1,13 @@
 
 
 type pet
+type pets
 (** The abstract type of values representing pets *)
 
-val from_json : Yojson.Basic.t -> pet
+
+
+val pet_of_json : Yojson.Basic.t -> pet
+val pets_of_json: Yojson.Basic.t -> pets
 
 val getHealth : pet -> int
 
