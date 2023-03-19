82aaa00d05d0bce60f2681c34628150715b47077
Edward He <eh538@cornell.edu>
2023-03-19 14:01:15 -0400

    Updated pet.from_json - Function for parsing a json and creating an instance of type pet

--- src/pet.ml
+++ src/pet.ml
@@ -1,9 +1,23 @@
 
-type pet
-(** The abstract type of values representing pets *)
+type pet = {
+  name: string;
+  gender: string;
+  description: string;
+  health: int;
+  hunger: int;
+  (*hygiene: int;*)
+}
 
-let from_json : Yojson.Basic.t -> pet = raise (Failure "not done")
+open Yojson.Basic.Util
 
+let from_json pet_json = 
+  {
+  name = pet_json |> member "name"  |> to_string;
+  gender = pet_json |> member "gender" |> to_string;
+  description = pet_json |> member "description" |> to_string;
+  health = pet_json |> member "health" |> to_int;
+  hunger = pet_json |> member "hunger" |> to_int; 
+  }
 let getHealth : pet -> string = raise (Failure "not done")
 
 let getHunger : pet -> string = raise (Failure "not done")
