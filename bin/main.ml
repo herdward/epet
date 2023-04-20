d42299ac2c135fb3e34c291ffe414f40843a7f7d
Edward He <eh538@cornell.edu>
2023-04-20 17:27:39 -0400

    modified pet functions to all return pet

--- bin/main.ml
+++ bin/main.ml
@@ -16,22 +16,21 @@ let rec check_if_bad_food food petfoodlist =
   | h :: t ->
       if Pet.get_bad_food_name h = food then true else check_if_bad_food food t
 
-(** this code doesn't appear to actually make any "edits" to the pet state, will
-    have to change, maybe return a pet object with the updated health? And have
-    another function to print the result?*)
-let bad_food_result food pet =
+let rec bad_food_result food pet =
   ANSITerminal.print_string [ ANSITerminal.red ]
     ("\n You selected to feed " ^ Pet.get_name pet ^ " with "
     ^ Pet.get_bad_food_name (Pet.get_bad_food pet food));
   print_endline
     ("\nAhh!" ^ " " ^ Pet.get_name pet
    ^ " did not like that! They lost health :(");
+  let updated_pet =
+    Pet.update_pet_health pet
+      (Pet.get_bad_food_effect (Pet.get_bad_food pet food))
+  in
   print_string
     ("\n" ^ Pet.get_name pet ^ " health is "
-    ^ string_of_int
-        (Pet.get_health
-           (Pet.update_pet_health pet
-              (Pet.get_bad_food_effect (Pet.get_bad_food pet food)))))
+    ^ string_of_int (Pet.get_health updated_pet));
+  updated_pet
 
 let food_result food pet =
   if check_if_bad_food food (Pet.get_bad_foods pet) then
@@ -39,7 +38,7 @@ let food_result food pet =
   else raise (Failure "Not a valid food")
 (*here should be the application of good_food_result or smth similar*)
 
-let encounter (pet : pet) : unit =
+let encounter (pet : pet) : pet =
   print_endline ("Looks like Amy Li forgot to feed " ^ Pet.get_name pet ^ "!!!");
   print_endline
     (Pet.get_name pet ^ " current health: " ^ string_of_int (Pet.get_health pet));
@@ -51,7 +50,7 @@ let encounter (pet : pet) : unit =
   match Stdlib.read_line () with
   | a -> food_result a pet
 
-let select_pet () =
+let select_pet () : pet =
   (* Print the game prompt *)
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\n\nWelcome to tomagachi game engine.\n";
@@ -66,5 +65,9 @@ let select_pet () =
       encounter pet
 
 (* Execute the game engine. *)
-
-let () = select_pet ()
+let () =
+  let updated_pet = select_pet () in
+  let updated_pet_name = Pet.get_name updated_pet in
+  let final_health = Pet.get_health updated_pet in
+  print_endline
+    ("\n" ^ updated_pet_name ^ " final health: " ^ string_of_int final_health)
